1b92dc8b70f47499ee2524732a2b39361bd46846
Victor Casemiro <victor.casemiro@dtidigital.com>
2024-07-14 12:06:33 -0400

    chore: Remove commented out code for accepting Cookies

--- tests/e2e/ea-play.spec.js
+++ tests/e2e/ea-play.spec.js
@@ -8,7 +8,7 @@ test('EA play by Menu to Steam', async ({ page }) => {
   await page.getByRole('link', { name: 'EA Play', exact: true }).click();
   await page.locator('a').filter({ hasText: /^Join Now$/ }).click();
   const page1Promise = page.waitForEvent('popup');
-  await page.getByRole('button', { name: 'Accept' }).click();
+  // await page.getByRole('button', { name: 'Accept' }).click();
   await expect(page.getByRole('link', { name: 'PlayStation' })).toBeVisible();
   await expect(page.getByRole('link', { name: 'Xbox' })).toBeVisible();
   await page.getByRole('link', { name: 'Steam' }).click();
@@ -28,7 +28,7 @@ test('EA Play by Home', async ({ page }) => {
   await expect.soft(page.locator('ea-section:nth-child(2) > .eapl-section-column > .eapl-animate__section-column')).toBeVisible();
   await expect.soft(page.getByText('R$ 90,00 / 3 monthsOr pay annually and save 44%R$ 200,00 / yearPlay select new-')).toBeVisible();
   await expect.soft(page.locator('ea-play-tile iron-icon').first()).toBeVisible();
-  await page.getByRole('button', { name: 'Accept' }).click();
+  // await page.getByRole('button', { name: 'Accept' }).click();
   await page.locator('a').filter({ hasText: /^Join EA Play$/ }).click();
   await page.getByRole('radio').check();
   await expect(page.getByLabel('R$ 200,00/yr 12 monthsBest')).toBeVisible();
--- tests/visual/ea-home.spec.js
+++ tests/visual/ea-home.spec.js
@@ -5,13 +5,13 @@ test.beforeEach(async ({ page }) => {
 });
 
 test('EA Play page should be equals to screenshot', async ({ page }) => {
-    await page.getByRole('button', { name: 'Accept' }).click();
+    // await page.getByRole('button', { name: 'Accept' }).click();
     await page.getByLabel('Menu', { exact: true }).click();
     await page.getByRole('link', { name: 'EA Play', exact: true }).click();
     await expect(page).toHaveScreenshot('EAPlayPage.png', { maxDiffPixels: 100 });
 });
 
 test('EA Home page should be equals to screenshot', async ({ page }) => {
-    await page.getByRole('button', { name: 'Accept' }).click();
+    // await page.getByRole('button', { name: 'Accept' }).click();
     await expect(page).toHaveScreenshot('HomePage.png', { maxDiffPixels: 100 });
 });
