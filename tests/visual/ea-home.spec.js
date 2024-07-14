36ec46df2e86c4f673988bd7d0a36a0adb3e4bf4
Victor Casemiro <victor.casemiro@dtidigital.com>
2024-07-14 11:51:47 -0400

    chore: Add visual regression tests for EA Home and EA Play pages

---
+++ tests/visual/ea-home.spec.js
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('https://www.ea.com/');
+});
+
+test('EA Play page should be equals to screenshot', async ({ page }) => {
+    await page.getByRole('button', { name: 'Accept' }).click();
+    await page.getByLabel('Menu', { exact: true }).click();
+    await page.getByRole('link', { name: 'EA Play', exact: true }).click();
+    await expect(page).toHaveScreenshot('EAPlayPage.png', { maxDiffPixels: 100 });
+});
+
+test('EA Home page should be equals to screenshot', async ({ page }) => {
+    await page.getByRole('button', { name: 'Accept' }).click();
+    await expect(page).toHaveScreenshot('HomePage.png', { maxDiffPixels: 100 });
+});
